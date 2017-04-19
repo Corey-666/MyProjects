5c9c5a5f035936393752f5352b54f3df553ebbfe
Corey-666 <coreykill666@gmail.com>
2017-04-19 16:07:27 +0300

    some examples of slices

--- TEST/slice_example.py
+++ TEST/slice_example.py
@@ -21,5 +21,45 @@ print(my_list[::-1]) # с конца
 
 print(my_list[1:4:1]) # срез с 1го по 3й (включительно) элементы
 
+################
 
+my_str = 'abcdifghijklmnop'
+print('My string: ', my_str)
+print('Каждый 2й элемент с 1го по 9й:', my_str[1:10:2],'\n',
+      'Каждый 2й элемент от начала до конца:', my_str[::2], '\n' # Если какой-то параметр НЕ указан, по умолчанию 0!
+      'С последнего элемента:', my_str[::-1], '\n' # [0:0:-1]
+      'В обратном порядке:', my_str[5:2:-1], '\n' # C 5го по 2й в обратном порядке
+      )
+for element in my_str:
+    print(element, end='-*-')
+print('\n')
+#    print(element, end='-*-') # Элементы строки через 1 заменяются -*-
+
+# Объект среза !!!
+sl = 'shit'
+print(sl[1:3])
+print(sl[slice(1, 3)])
+print(sl[::-1])
+print(sl[slice(None, None, -1)], '\n')
+
+######################
+'''
+name = input('Enter yor name: ')
+print('{0} You are stupid {1}'.format(name, 'bitch'))
+'''
+# replace method!
+sss = 'v;wdv;vjqwvwc;ckwjjckk'
+print(sss.replace('w', '-*-'), '\n') # По сути replace выполняет глобальный поиск с заменой!!!
+
+############################
+
+#join method
+string = 'CoreyKill'
+print(string)
+ls = list(string)
+ls = ls + ['666']
+print(ls)
+print( ''.join(ls)) # Метод join собирает список обратно в строку!
+
+##############################
 
