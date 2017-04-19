d87141b1ddf52496eebd2433107008e573afd8e9
Corey-666 <coreykill666@gmail.com>
2017-04-19 16:05:30 +0300

    =)

--- TEST/test.py
+++ TEST/test.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3.5
 # -*- coding: utf-8 -*-
 __author__ = "cOREY_kILL"
 __license__ = "GPL"
@@ -13,6 +13,7 @@ def shit():
             s = 'exit'
             break
 '''
+
 '''
 x = 666 # Глобальная переменная
 
@@ -25,6 +26,7 @@ foo(x)
 print(x) # Вне тела функции х остался прежним!
 
 '''
+
 '''
 def max(x, y):
     if x < y:
@@ -45,6 +47,7 @@ for i in sys.argv:
 print(sys.argv)
 
 '''
+
 '''
 elif sys.argv == '-open':
     print('OPEN BLEAT')
@@ -53,7 +56,57 @@ print('\n\nPATH is ', sys.path,'\n')
 
 '''
 
-import os
+'''
+print(2<4<6)
 
-dir = '/home/$USER/Temp/dir_test'
-print(os.path.abspath(dir))
+print('{0:o}, {1:x}, {2:b}'.format(100,100,100)) # o - восьмиричный литерал
+                                                 # x - шестнадцетиричный
+                                                 # b - двоичный
+
+
+print(int('01010101', 2)) # Преобразование изображения числа в число по основанию
+
+a = 666
+print(a.bit_length())
+
+print(0.1 + 0.1 + 0.1 - 0.3)
+'''
+
+
+
+# Экранированные и НЕ форматированные строки
+'''
+s = set('sdlvmrogij3490tugebjqp"})"/') # Множество
+print('a' in s,'\n', s, '\n')
+
+print('Ж\t0\nП \x00\tA') # экранированные последовательности
+
+print('C:\Windows\Shit') # Правильно писать с \\ (двумя слэшами) или r перед строкой!!!
+
+path = 'c:\shit\fuck\ass' # НЕ правильно
+print(path)
+path_right = r'c:\shit\fuck\ass'
+print(path_right)
+'''
+
+print(r'JOPA'[:-1])
+
+minus = (len('jopa')-1) # !!! Технически отрицательное смещение складывается с длинной строки!!!
+print('jopa'[minus])    # len('jopa')-1 = 4+(-1) = 3
+print('jopa'[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
